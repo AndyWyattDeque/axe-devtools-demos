d50a5205f22d42a9de43a55a92c32f17ba4718ac
Harris Schneiderman <schne324@gmail.com>
2023-05-16 07:58:55 -0700

    test: gets @axe-core/watcher and axe DeveloperHub setup and running in CI (#8)

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,21 @@
 const { defineConfig } = require('cypress')
+const { cypressConfig } = require('@axe-core/watcher')
+const assert = require('assert')
 
-module.exports = defineConfig({
-  e2e: {
-    specPattern: './test/cypress/*.js',
-    supportFile: false,
-  },
-})
+const { SERVER_URL, API_KEY } = process.env
+
+assert(SERVER_URL, 'SERVER_URL is required')
+assert(API_KEY, 'API_KEY is required')
+
+module.exports = defineConfig(
+  cypressConfig({
+    axe: {
+      apiKey: API_KEY,
+      serverURL: SERVER_URL,
+    },
+    e2e: {
+      specPattern: './test/cypress/*.js',
+      supportFile: './test/support.js',
+    },
+  })
+)
