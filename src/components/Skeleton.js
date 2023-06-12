a40e9118dd697c79b6b7c1b83b20b9c2ffdef9a1
Harris Schneiderman <schne324@gmail.com>
2023-06-12 13:54:31 -0700

    feat: adds active indication to active nav item  (#57)

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,45 +1,61 @@
 import React from 'react'
-import { Layout, TopBarItem, Icon } from '@deque/cauldron-react'
-import { Link, Outlet } from 'react-router-dom'
+import { Layout, NavItem, Icon, NavBar } from '@deque/cauldron-react'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 import './Skeleton.css'
 
-const Skeleton = () => (
-  <div className="App">
-    <nav className="TopBar">
-      <ul>
-        <TopBarItem>
-          <Icon type="clipboard" />
+const Skeleton = () => {
+  const location = useLocation()
+  const paths = React.useMemo(() => {
+    const { pathname } = location
+
+    return {
+      dashboardIsActive: pathname === '/',
+      blogIsActive: pathname.startsWith('/blog'),
+      settingsIsActive: pathname === '/settings',
+    }
+  }, [location])
+
+  return (
+    <div className="App">
+      <NavBar className="TopBar">
+        <NavItem active={paths.dashboardIsActive}>
+          <Icon type="info-circle" />
           <Link to="/">Dashboard</Link>
-        </TopBarItem>
-        <TopBarItem>
+        </NavItem>
+        <NavItem active={paths.blogIsActive}>
           <Icon type="clipboard" />
           <Link to="/blog">Blog</Link>
-        </TopBarItem>
-        <TopBarItem className="Align--right">
+        </NavItem>
+        <NavItem
+          active={paths.settingsIsActive}
+          className={`NavItem Align--right ${
+            paths.settingsIsActive ? 'NavItem--active' : ''
+          }`}
+        >
           <Icon type="gears" />
           <Link to="/settings">Settings</Link>
-        </TopBarItem>
-      </ul>
-    </nav>
-    <Layout>
-      <Outlet />
-    </Layout>
-    <footer className="Footer">
-      <p>Awesome Recipes Inc.</p>
-      <div>
-        <a href="https://github.com/dequelabs">
-          <Icon type="github" />
-        </a>
-        <a href="https://linkedin.com/deque">
-          <Icon type="linkedin" />
-        </a>
-      </div>
-      <div>
-        <label className="Field__label">Site Search</label>
-        <input type="text" className="Field__text-input" />
-      </div>
-    </footer>
-  </div>
-)
+        </NavItem>
+      </NavBar>
+      <Layout>
+        <Outlet />
+      </Layout>
+      <footer className="Footer">
+        <p>Awesome Recipes Inc.</p>
+        <div>
+          <a href="https://github.com/dequelabs">
+            <Icon type="github" />
+          </a>
+          <a href="https://linkedin.com/deque">
+            <Icon type="linkedin" />
+          </a>
+        </div>
+        <div>
+          <label className="Field__label">Site Search</label>
+          <input type="text" className="Field__text-input" />
+        </div>
+      </footer>
+    </div>
+  )
+}
 
 export default Skeleton
