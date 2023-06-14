76a646ab869e43fc3aa950bbd980549b54fb5679
Harris Schneiderman <schne324@gmail.com>
2023-06-13 20:19:42 -0700

    feat: better blog post styling (#60)

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Main, Icon } from '@deque/cauldron-react'
+import { Main, Icon, FieldWrap } from '@deque/cauldron-react'
 import { Link, useParams } from 'react-router-dom'
 import blogData from '../blog-data'
 import './BlogPost.css'
@@ -15,12 +15,14 @@ const BlogPost = () => {
       <Main id="main-content" aria-labelledby="main-heading" tabIndex={-1}>
         {data ? (
           <div>
-            <Link className="link" to="/blog">
+            <Link className="Link Back" to="/blog">
               <Icon type="arrow-circle-left" />
               Back to blog
             </Link>
-            <h1 id="main-heading">{data.title}</h1>
-            {data.content}
+            <FieldWrap>
+              <h1 id="main-heading">{data.title}</h1>
+              {data.content}
+            </FieldWrap>
           </div>
         ) : (
           <h1 id="main-heading">Blog post not found</h1>
