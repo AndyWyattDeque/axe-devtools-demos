4c74622ae59c5feee9fc470849b687a6bdc2020c
Harris Schneiderman <schne324@gmail.com>
2023-06-16 13:46:48 -0700

    Adds issue to be found with manual testing (remaining testing in axe Pro or manual testing in auditor) (#61)

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,24 +1,44 @@
 import React from 'react'
-import { Main, Icon, FieldWrap } from '@deque/cauldron-react'
+import { Main, Icon, FieldWrap, Button } from '@deque/cauldron-react'
 import { Link, useParams } from 'react-router-dom'
 import blogData from '../blog-data'
 import './BlogPost.css'
 
+// display message for 4 seconds
+const MESSAGE_DURATION = 4000
+
 const BlogPost = () => {
   const { id } = useParams()
+  const [showReported, setShowReported] = React.useState(false)
   const data = React.useMemo(() => {
     return blogData.find((b) => b.id === id)
   }, [id])
+  const onReportOffensiveClick = () => {
+    setShowReported(true)
+    setTimeout(() => setShowReported(false), MESSAGE_DURATION)
+  }
 
   return (
     <div className="Blog">
       <Main id="main-content" aria-labelledby="main-heading" tabIndex={-1}>
+        {showReported && (
+          <div className="Reported">
+            <Icon type="caution" />
+            This blog post has been reported for having offensive content. It
+            will be reviewed by our team and removed if found to be offensive.
+          </div>
+        )}
         {data ? (
           <div>
-            <Link className="Link Back" to="/blog">
-              <Icon type="arrow-circle-left" />
-              Back to blog
-            </Link>
+            <div className="Blog__links">
+              <Link className="Link Back" to="/blog">
+                <Icon type="arrow-circle-left" />
+                Back to blog
+              </Link>
+              <Button onClick={onReportOffensiveClick} variant="link">
+                Report offensive content
+              </Button>
+            </div>
             <FieldWrap>
               <h1 id="main-heading">{data.title}</h1>
               {data.content}
